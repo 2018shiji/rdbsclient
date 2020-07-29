84760901ab7fa923d321228ab41e1be988644310
lizhuangjie.chnet <lizhuangjie.chnet@szcmml.com>
2020-07-29 17:09:50 +0800

    commit spring data jpa

--- src/main/java/com/module/dataAccesser/DataAccesserApplication.java
+++ src/main/java/com/module/dataAccesser/DataAccesserApplication.java
@@ -2,8 +2,6 @@ package com.module.dataAccesser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DataAccesserApplication {
--- src/test/java/com/module/dataAccesser/DataAccesserApplicationTests.java
+++ src/test/java/com/module/dataAccesser/DataAccesserApplicationTests.java
@@ -1,6 +1,5 @@
 package com.module.dataAccesser;
 
-import com.module.dataAccesser.config.abcdefg;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
